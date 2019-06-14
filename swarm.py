50c4336196055b13d3ee75164aa194e3e69b1fde
AleksanderSz <olonet@tlen.pl>
2019-06-14 15:06:42 +0200

    swaaarm

---
+++ swarm.py
@@ -0,0 +1,153 @@
+import numpy as np
+import random as rand
+from operator import attrgetter
+
+class FoodSource(object):
+    trials = 0
+
+    """docstring for FoodSource"""
+    def __init__(self, initial_solution, initial_fitness):
+        super(FoodSource, self).__init__()
+
+        self.solution = initial_solution
+        self.fitness = initial_fitness
+
+    def __repr__(self):
+        return f'<FoodSource s:{self.solution} f:{self.fitness} />'
+
+
+class ABC(object):
+    """docstring for ABC"""
+
+    food_sources = []
+
+    def __init__(
+        self,
+        npopulation,
+        nruns,
+        fn_eval,
+        *,
+        trials_limit=50,
+        employed_bees_percentage=0.5,
+        fn_lb=[-5, -5],
+        fn_ub=[5, 5],
+    ):
+        super(ABC, self).__init__()
+        self.npopulation = npopulation
+        self.nruns = nruns
+        self.fn_eval = fn_eval
+        self.trials_limit = trials_limit
+        self.fn_lb = np.array(fn_lb)
+        self.fn_ub = np.array(fn_ub)
+
+        self.employed_bees = round(npopulation * employed_bees_percentage)
+        self.onlooker_bees = npopulation - self.employed_bees
+
+    def optimize(self):
+        self.initialize()
+
+
+        for nrun in range( self.nruns+1):
+            self.employed_bees_stage()
+            self.onlooker_bees_stage()
+            self.scout_bees_stage()
+            yield np.array([e.solution for e in self.food_sources]), None
+
+
+    def initialize(self):
+        self.food_sources = [self.create_foodsource() for i in range(self.employed_bees)]
+
+    def employed_bees_stage(self):
+        for i in range(self.employed_bees):
+            food_source = self.food_sources[i]
+            new_solution = self.generate_solution(i)
+            best_solution = self.best_solution(food_source.solution, new_solution)
+
+            self.set_solution(food_source, best_solution)
+
+    def onlooker_bees_stage(self):
+        for i in range(self.onlooker_bees):
+            probabilities = [self.probability(fs) for fs in self.food_sources]
+            selected_index = self.selection(range(len(self.food_sources)), probabilities)
+            selected_source = self.food_sources[selected_index]
+            new_solution = self.generate_solution(selected_index)
+            best_solution = self.best_solution(selected_source.solution, new_solution)
+
+            self.set_solution(selected_source, best_solution)
+
+    def scout_bees_stage(self):
+        for i in range(self.employed_bees):
+            food_source = self.food_sources[i]
+
+            if food_source.trials > self.trials_limit:
+                food_source = self.create_foodsource()
+
+
+    def generate_solution(self, current_solution_index):
+        solution = self.food_sources[current_solution_index].solution
+        k_source_index = self.random_solution_excluding([current_solution_index])
+        k_solution = self.food_sources[k_source_index].solution
+        d = rand.randint(0, len(self.fn_lb) - 1)
+        r = rand.uniform(-1, 1)
+
+        new_solution = np.copy(solution)
+        new_solution[d] = solution[d] + r * (solution[d] - k_solution[d])
+
+        return np.around(new_solution, decimals=4)
+
+    def random_solution_excluding(self, excluded_index):
+        available_indexes = set(range(self.employed_bees))
+        exclude_set = set(excluded_index)
+        diff = available_indexes - exclude_set
+        selected = rand.choice(list(diff))
+
+        return selected
+
+    def best_solution(self, current_solution, new_solution):
+        if self.fitness(new_solution) > self.fitness(current_solution):
+            return new_solution
+        else:
+            return current_solution
+
+    def probability(self, solution_fitness):
+        fitness_sum = sum([fs.fitness for fs in self.food_sources])
+        probability = solution_fitness.fitness / fitness_sum
+
+        return probability
+
+    def fitness(self, solution):
+        result = self.fn_eval(solution)
+
+        if result >= 0:
+            fitness = 1 / (1 + result)
+        else:
+            fitness = abs(result)
+
+        return fitness
+
+    def selection(self, solutions, weights):
+        return rand.choices(solutions, weights)[0]
+
+    def set_solution(self, food_source, new_solution):
+        if np.array_equal(new_solution, food_source.solution):
+            food_source.trials += 1
+        else:
+            food_source.solution = new_solution
+            food_source.trials = 0
+
+    def best_source(self):
+        best = max(self.food_sources, key=attrgetter('fitness'))
+
+        return best
+
+    def create_foodsource(self):
+        solution = self.candidate_solution(self.fn_lb, self.fn_ub)
+        fitness = self.fitness(solution)
+
+        return FoodSource(solution, fitness)
+
+    def candidate_solution(self, lb, ub):
+        r = rand.random()
+        solution = lb + (ub - lb) * r
+
+        return np.around(solution, decimals=4)
